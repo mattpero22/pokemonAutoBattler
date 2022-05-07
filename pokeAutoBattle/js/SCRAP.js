e186e0bea7e1ce72d685c84f91cf930464133c45
Matthew Pero <mpero@macbook-pro.myfiosgateway.com>
2022-05-07 09:06:27 -0400

    tweaks on html and ideatxt. created a function to return the ajax promise for a pokemon. Can then access that pokemon by using .then() and then parsing data.

--- pokeAutoBattle/js/SCRAP.js
+++ pokeAutoBattle/js/SCRAP.js
@@ -1,6 +1,55 @@
-URL = "https://pokeapi.co/api/v2/pokemon/bulbasaur"
+// Useful constant links to API
+const pokedexURL = "https://pokeapi.co/api/v2/pokedex/national";
+const pokemonURL = "https://pokeapi.co/api/v2/pokemon/1";
 
-$.ajax(URL).then(function(data){
-    console.log(data),
-    function() {console.log("error")}
+// JQ objs
+let $body = $('body')
+let $pokeName = $('#poke-name')
+
+// function createPokemonCard() {
+//     $.ajax(pokemonURL).then(function(data){
+//         console.log(data)
+//         $body.append(`<p>${data.name}</p>`)
+//     })
+// }
+
+// createPokemonCard();
+// console.log(document.querySelector('p'))
+let x = {};
+
+function getPokemon(num) {
+    $.extend(
+        x,             // var to store retrieved data in
+        $.ajax(pokemonURL).then(
+            function(data){
+                return data
+            }
+        )
+    )
+}
+
+getPokemon(1);
+console.log(x)
+x.then(function(data){
+    console.log(data.name)
 })
+// console.log(typeof(newPokemon))
+
+
+// x = jQuery.extend(
+//     {getValues: function(url) {
+//         var result = null;
+//         $.ajax({
+//             url: pokemonURL,
+//             type: 'get',
+//             dataType: 'xml',
+//             async: false,
+//             success: function(data) {
+//                 result = data;
+//             }
+//         });
+//        return result;
+//     }
+// });
+
+
