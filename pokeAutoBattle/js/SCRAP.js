340d7f78f1e2508de48f380bfca83dd667fec168
Matthew Pero <mpero@macbook-pro.myfiosgateway.com>
2022-05-08 11:06:47 -0400

    pushing before branch to work on specific feature.

--- pokeAutoBattle/js/SCRAP.js
+++ pokeAutoBattle/js/SCRAP.js
@@ -6,42 +6,7 @@ const pokemonURL = "https://pokeapi.co/api/v2/pokemon/1";
 let $body = $('body')
 let $pokeName = $('#poke-name')
 
-// function createPokemonCard() {
-//     $.ajax(pokemonURL).then(function(data){
-//         console.log(data)
-//         $body.append(`<p>${data.name}</p>`)
-//     })
-// }
-
-// createPokemonCard();
-// console.log(document.querySelector('p'))
-// let temp = {};
-// let pokemon = {
-//     name: "",
-// };
-
-// function getPokemon(num) {
-//     $.extend(
-//         temp,             // var to store retrieved data in
-//         $.ajax(pokemonURL).then(    // item to store in that variable
-//             function(data){
-//                 return data
-//             }
-//         )
-//     )
-// }
-
-// getPokemon(1);
-// console.log(temp)
-// temp.then(function(data) {
-//     console.log(pokemon)
-//     pokemon.name = data.name;
-// })
-// console.log(pokemon)
-// console.log(typeof(newPokemon))
-
-
-x = jQuery.extend(
+jQuery.extend(
     {getValues: function(url) {
         var result = null;
         $.ajax({
