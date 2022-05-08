50ed862eeb8edb03727402e8a996a3189a38744f
Matthew Pero <mpero@macbook-pro.myfiosgateway.com>
2022-05-08 19:16:05 -0400

    made some class assignments, added a bit of text, and centered text on startmenu

--- pokeAutoBattle/js/startpage.js
+++ pokeAutoBattle/js/startpage.js
@@ -1,2 +1,8 @@
-$playBtn = $('#play-btn')
-$playBtn.click(() => location.href = "https://pokeapi.co/")
+// store elements as JQ
+$playBtn = $('#play-btn')       // starts game
+$rulesBtn = $('#rules-btn')     // displays the rule frame
+
+// if play btn clicked, set it to target firstpick.html
+$playBtn.click(() => location.href = "./firstpick.html")
+
+
