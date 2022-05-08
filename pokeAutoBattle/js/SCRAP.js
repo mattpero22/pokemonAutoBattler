74a678bdb20ab1b6af81b91f61c6553bfa3c07c4
Matthew Pero <mpero@macbook-pro.myfiosgateway.com>
2022-05-08 16:18:43 -0400

    added comments on json and did basic formatting of html and css for start menu page

--- pokeAutoBattle/js/SCRAP.js
+++ pokeAutoBattle/js/SCRAP.js
@@ -2,17 +2,15 @@
 const pokedexURL = "https://pokeapi.co/api/v2/pokedex/national";
 const pokemonURL = "https://pokeapi.co/api/v2/pokemon/";
 
-// JQ objs
-let $body = $('body')
-
-// Function to retrieve a pokemon and store as a local variable
+// Function to retrieve a pokemon by number and store as a obj
 function getPokemonByNum(number) {
-    return $.parseJSON($.ajax({
-        url: pokemonURL + number,
-        async: false,
+    return $.parseJSON($.ajax({ //parse return of ajax.responseText as JSON to get obj back
+        url: pokemonURL + number, //pass args for ajax call as an obj to customize it more, this line does url
+        async: false,   // makes ajax execute in sync s.t. we have the pokemon we need to move on
     }).responseText);
 }
 
+// Function to retrieve a pokemon by name and store as a obj
 function getPokemonByName(name) {
     return $.parseJSON($.ajax({
         url: pokemonURL + name,
