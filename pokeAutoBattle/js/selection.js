2b3659bffc985c3908ba7f6789d2c7f7a0fa156f
Matthew Pero <mpero@macbook-pro.myfiosgateway.com>
2022-05-08 21:16:44 -0400

    added button to take me back to the main page (dev use for now)

--- pokeAutoBattle/js/selection.js
+++ pokeAutoBattle/js/selection.js
@@ -3,8 +3,10 @@ $.getScript("pabFunctions.js")
 // store elements as JQ
 
 $(window).on('load', function() {
-    $('body').fadeOut(0, () => console.log("page loaded"))
+    $('body').fadeOut(0)
+    $('body').append('<input type="button" value="Cancel">').click(()=>location.href="./index.html")
     $('body').fadeIn(1000, function() {
+
     })
 })
 
