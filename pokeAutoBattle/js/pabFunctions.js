a0cc0bd3c74fc248f614f46f6e7aaf0e57241cea
Matthew Pero <mpero@macbook-pro.myfiosgateway.com>
2022-05-09 13:18:34 -0400

    made a function to filter the starting choices for the user so you dont start with an OP mon

--- pokeAutoBattle/js/pabFunctions.js
+++ pokeAutoBattle/js/pabFunctions.js
@@ -1,12 +1,26 @@
 // Useful constant links to API
 const pokedexURL = "https://pokeapi.co/api/v2/pokedex/";
 const pokemonURL = "https://pokeapi.co/api/v2/pokemon/";
+const evolutionURL = "https://pokeapi.co/api/v2/evolution-chain/";
+
+// Function to change screen
+function changeScreen() {
+    $('body').fadeOut(0)
+    $('body').fadeIn(1000)
+}
 
 // Function to retrieve a pokemon by number and store as a obj
 function getPokemonByNum(number) {
     return $.parseJSON($.ajax({ //parse return of ajax.responseText as JSON to get obj back
         url: pokemonURL + number, //pass args for ajax call as an obj to customize it more, this line does url
         async: false,   // makes ajax execute in sync s.t. we have the pokemon we need to move on
+    }).responseText);
+}
+
+function getPokemonSpeciesByURL(speciesURL) {
+    return $.parseJSON($.ajax({
+        url: speciesURL,
+        async: false,
     }).responseText);
 }
 
@@ -26,8 +40,32 @@ function getPokedexByRegion(region) {
     }).responseText)
 }
 
-// Function to change screen
-function changeScreen() {
-    $('body').fadeOut(0)
-    $('body').fadeIn(100)
+// Function to identify the evolutionary stage of a pokemon when passed it's id
+function getPokemonEvoChainByURL(evoChainURL) {
+    return $.parseJSON($.ajax({
+        url: evoChainURL,
+        async: false,
+    }).responseText);
+}
+
+// Get valid choice pokemon
+function getValidPokemonChoice(region) {
+    let validChoice = false;
+    pokemonRange = getPokedexByRegion(region).pokemon_entries.length;
+    while (validChoice === false) {
+        let pokemon = getPokemonByNum(Math.floor(Math.random() * pokemonRange));
+        let pokeSpec = getPokemonSpeciesByURL(pokemon.species.url);
+        console.log(pokemon.id, pokemon.name, pokeSpec)
+        if (pokeSpec.is_legendary === false && pokeSpec.is_mythical === false) {
+            let pokeEvoChain = getPokemonEvoChainByURL(pokeSpec.evolution_chain.url);
+            if (pokeEvoChain.chain.isBaby === true) {       // if its a baby pokemon its first evo
+                validChoice = true;
+                return pokemon
+            }
+            else if(pokeEvoChain.chain.species.name === pokemon.name) {
+                validChoice = true;
+                return pokemon
+            } 
+        }
+    }
 }
--- pokeAutoBattle/js/selection.js
+++ pokeAutoBattle/js/selection.js
@@ -1,19 +1,30 @@
+// game variables
+let pokemon1 = null;
+let pokemon2 = null;
+let pokemon3 = null;
+let validChoice1 = false;
+let validChoice2 = false;
+let validChoice3 = false;
+let regionType = 'national';
+
 // jQ objects
 $card1 = $('#card1')
 $card2 = $('#card2')
 $card3 = $('#card3')
+$selectmenu = $("select-menu")
 
 // fadeIn on load for a nicer effect
 $(window).on('load', changeScreen())
 $('body').click(()=>location.href="./index.html")
 
-// get the highest allowed number for the pokemon and them get 3 randomly
-pokemonRange = getPokedexByRegion('national').pokemon_entries.length;
-pokemon1 = getPokemonByNum(Math.floor(Math.random() * pokemonRange))
-pokemon2 = getPokemonByNum(Math.floor(Math.random() * pokemonRange))
-pokemon3 = getPokemonByNum(Math.floor(Math.random() * pokemonRange))
-console.log(pokemon1, pokemon2, pokemon3)
+// get the highest allowed number for the pokemon and then get 3 randomly
+pokemon1 = getValidPokemonChoice(regionType);
+pokemon2 = getValidPokemonChoice(regionType);
+pokemon3 = getValidPokemonChoice(regionType);
+console.log(pokemon1.name, pokemon2.name, pokemon3.name)
 
 $card1.append(`<p>${pokemon1.name}<p>`, `<img src="${pokemon1.sprites.front_default}"/>`)
 $card2.append(`<p>${pokemon2.name}<p>`, `<img src="${pokemon2.sprites.front_default}"/>`)
 $card3.append(`<p>${pokemon3.name}<p>`, `<img src="${pokemon3.sprites.front_default}"/>`)
+
+getValidPokemonChoice(regionType);
