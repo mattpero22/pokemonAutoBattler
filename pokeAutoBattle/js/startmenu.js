c3240eb7d917f7fb1a78355bb3330d5d2984570d
Matthew Pero <mpero@macbook-pro.myfiosgateway.com>
2022-05-10 01:31:50 -0400

    lots of progress, should have committed more often

--- pokeAutoBattle/js/startmenu.js
+++ pokeAutoBattle/js/startmenu.js
@@ -2,6 +2,7 @@
 $body = $('body')
 $playBtn = $('#play-btn')       // starts game
 $rulesBtn = $('#rules-btn')     // displays the rule frame
+$themeBtn = $('#theme-btn')     // toggles day and night theme
 
 // fade in on load
 $(window).on('load', fadeScreen())
@@ -12,6 +13,12 @@ $playBtn.click(function(){
         location.href = "./selection.html";
     })
 })
-        
+
+$themeBtn.click(function(){
+    $body.fadeOut(1000, function(){
+        $body.css("background-image", "url(./css/images/night.jpg)")
+        $body.fadeIn(1000)
+    })
+})
 
 
